719d3c9a0b389f3933cc94d6c5dd4639a896043b
McKay1717 <nicolas1706ni@gmail.com>
2020-04-29 17:28:21 +0200

    Remove tasker and move to supervisord + cron

---
+++ rootfs/scripts/start_peering_manager.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+/peering-manager/scripts/upgrade.sh
+
+if [ ! -f "/config/install.lock" ]; then
+
+    python3 manage.py createsuperuser --no-input
+    touch /config/install.lock
+fi
+
+#Start Peering Manager
+python3 manage.py runserver 0.0.0.0:8000 --insecure
+
